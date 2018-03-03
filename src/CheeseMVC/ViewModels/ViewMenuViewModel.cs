b51668b033a89f5cf3d4dcb7030f25a2ea8459a9
KJ Sweet <ka.g.sweet@gmail.com>
2018-03-02 19:32:36 -0600

    Implimented Menus and? I think it's done?

---
+++ src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class ViewMenuViewModel
+    {
+        public IList<CheeseMenu> Items { get; set; }
+        public Menu Menu { get; set; }
+
+        public ViewMenuViewModel()
+        {
+
+        }
+    }
+}
