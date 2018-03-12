71561d8779440b492a5bd48e459dd0aadc74665a
KJ Sweet <ka.g.sweet@gmail.com>
2018-03-11 19:48:51 -0500

    COmplete!

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -43,7 +43,7 @@ namespace CheeseMVC.Controllers
                 context.Menus.Add(newMenu);
                 context.SaveChanges();
 
-                return Redirect("/Menu");
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
             }
 
             return View(addMenuViewModel);
